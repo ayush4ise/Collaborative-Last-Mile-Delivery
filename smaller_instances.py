0d99688c60d9f857cbcf2abb010f18c0bfe8b26f
ayush4ise <pkumar46802@gmail.com>
2024-11-03 21:15:43 +0530

    current code does better for A3
    it allows direct visit to collaboration point without finishing original deliveries first

--- smaller_instances.py
+++ smaller_instances.py
@@ -233,7 +233,7 @@ for v in V:
             for s in S:
                 for e in S:
                     if s!=e:
-                        m.addConstr(M * (4 - Q_c_s[c, s] - Y_c_v[c, v] - B_v_s[v, e] - K_sv_o[e, v, o]) + gp.quicksum(X_ij_v[f, c, v] for f in C2+O) + X_ij_v[o, c, v] >= 2, name=f"SecondEchelon11_v_{v}_o_{o}_c_{c}_s_{s}_e_{e}")
+                        m.addConstr(M * (4 - Q_c_s[c, s] - Y_c_v[c, v] - B_v_s[v, e] - K_sv_o[e, v, o]) + gp.quicksum(X_ij_v[f, c, v] for f in C2+O)  >= 1, name=f"SecondEchelon11_v_{v}_o_{o}_c_{c}_s_{s}_e_{e}")
 
 for v in V:
     for o in O:
@@ -241,7 +241,7 @@ for v in V:
             for s in S:
                 for e in S:
                     if s!=e:
-                        m.addConstr(M * (4 - Q_c_s[c, s] - Y_c_v[c, v] - B_v_s[v, e] - K_sv_o[e, v, o]) + gp.quicksum(X_ij_v[f, c, v] for f in C1+O) + X_ij_v[o, c, v] >= 2, name=f"SecondEchelon12_v_{v}_o_{o}_c_{c}_s_{s}_e_{e}")
+                        m.addConstr(M * (4 - Q_c_s[c, s] - Y_c_v[c, v] - B_v_s[v, e] - K_sv_o[e, v, o]) + gp.quicksum(X_ij_v[f, c, v] for f in C1+O)   >= 1, name=f"SecondEchelon12_v_{v}_o_{o}_c_{c}_s_{s}_e_{e}")
 
 
 for v in V:
